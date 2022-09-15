14c9524e9af40e91f095ab2cc611be0b630de87e
Israel Miller <israel.miller@stonybrook.edu>
2022-09-15 15:02:52 -0400

    basic flask app w routes, blank html & css files

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def default_home():
+    return render_template('home_page.html')
+
+@app.route('/patients')
+def patients():
+    return render_template('patients_page.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
